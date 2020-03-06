0cd8d37a118a0bd3315a0731cb72cc0f26564c32
Artem Drushchyts <temafm-3@yandex.ru>
2020-03-06 21:36:01 +0300

    feat: complete morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,14 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let key = '';
+    let result = '';
+    for(let i = 0; i < expr.length; i = i + 10) {
+        key = expr.slice(i, i+10).replace(/(11)/g , '-').replace(/(10)/g , '.').replace(/(00)/g , '');
+        // console.log(key, MORSE_TABLE[key]);
+        (key === '**********' ? result = result + ' ' : result = result + MORSE_TABLE[key])
+    }
+    return result;
 }
 
 module.exports = {
